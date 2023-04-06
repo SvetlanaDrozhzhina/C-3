751f2d2555d7adf71a1b5271fb8f8acaee7a9692
Дрожжина Светлана <arianna-14@mail.ru>
2023-04-06 11:13:26 +0300

    Создан task 21

---
+++ Task 21/Program.cs
@@ -0,0 +1,16 @@
+﻿int x1 = ReadInt("Введите х для точки 1: ");
+int y1 = ReadInt("Введите y для точки 1: ");
+int z1 = ReadInt("Введите z для точки 1: ");
+int x2 = ReadInt("Введите х для точки 2: ");
+int y2 = ReadInt("Введите y для точки 2: ");
+int z2 = ReadInt("Введите z для точки 2: ");
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z2 - z1;
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine("Расстояние = " + length);
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
