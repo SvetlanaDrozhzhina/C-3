348fa3b8674cb9836d7e967e5e83eecc72d46a38
Дрожжина Светлана <arianna-14@mail.ru>
2023-04-06 10:49:30 +0300

    Создан nask 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+void CheckingNumber(string number){
+    if (number[0]==number[4] || number[1]==number[3]){
+        Console.WriteLine($"число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"число {number} - не палиндром.");
+}
+if (number!.Length == 5){
+    CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
